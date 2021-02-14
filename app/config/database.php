bb47c88361292e26aa7ac7b2e7ca8da3ef4e2a76
Christopher Edynak <christopher1edynak@gmail.com>
2021-02-14 14:35:27 -0500

    implemented pdo connection to db

--- app/config/database.php
+++ app/config/database.php
@@ -1,3 +1,21 @@
 <?php
+    $host = "localhost";
+    $dbname = "cmslogin";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dsn = "mysql:host=".$host.";dbname=".$dbname;
 
+    //establish connection to database
+    try{
+        $dbc = new PDO($dsn, $dbusername, $dbpassword);
+        //PDO::ERRMODE_SILENT
+        $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbc->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        //check connection to database
+        echo "You have successfully connected to the database named ".$dbname;
+
+    }catch(PDOException $ex){
+        //get error message
+        echo "Connection failed: ".$ex->getMessage();
+    }
 ?>
--- app/public/front/home.inc.php
+++ app/public/front/home.inc.php
@@ -1,3 +1,9 @@
 <?php
 
 ?>
+
+    <div class="row">
+        <div class="col-sm-12">
+            This is the home page
+        </div>
+    </div>
